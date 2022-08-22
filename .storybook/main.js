7d68b5122a854603c7655ff3e08595e84a7550ff
cherr0 <adsl1664@gmail.com>
2022-08-22 23:37:35 +0900

    Change: mdx 사용 환경 세팅

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,34 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  framework: '@storybook/react',
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/components/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-actions',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app'
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  typescript: {
+    check: false,
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true
+    }
+  },
+  core: {
+    builder: '@storybook/builder-webpack5'
+  },
+  webpackFinal: async (config) => {
+    config.plugins.push(new TsconfigPathsPlugin({}))
+
+    return config
   }
 }
