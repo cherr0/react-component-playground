a4097bb8a769c80587a2bdc166e4313b86166378
Peter Park <adsl1664@gmail.com>
2023-07-19 16:36:49 +0900

    feat: 토큰 정보 업데이트

---
+++ src/styles/mixin.ts
@@ -0,0 +1,8 @@
+import { css } from 'styled-components';
+
+export const noneDraggable = css`
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+`;
